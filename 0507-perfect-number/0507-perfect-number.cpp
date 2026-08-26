9aa77faea8dae9e34a787b3338eb52b96b5c040c
Praveen Gupta <mrpraveengupta077@gmail.com>
2026-08-26 10:22:29 +0530

    Time: 1058 ms (36.54%), Space: 7.6 MB (88.58%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum=1 ;
+        if(num==1){
+            return false;
+        }
+        for(int i=2 ;i<=(num/2);i++){
+            if((num%i)==0){
+                sum+=i;
+            }
+        }
+        if(sum==num){
+            return true;
+        }
+        return false;
+    }
+};
