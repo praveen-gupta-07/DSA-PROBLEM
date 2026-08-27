b94fee65c977e2930d691a5f8dffa94769c3f9c6
Praveen Gupta <mrpraveengupta077@gmail.com>
2026-08-27 20:32:46 +0530

    Time: 83 ms (20.67%), Space: 111.4 MB (28.11%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> temp;
+        for(int i = 0; i < nums.size(); i++) {
+            temp[nums[i]]++;
+
+            if(temp[nums[i]] >= 2) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
