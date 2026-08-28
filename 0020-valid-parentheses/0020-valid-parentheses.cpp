fb20d941297a56d40db60ecf371254cecb72322f
Praveen Gupta <mrpraveengupta077@gmail.com>
2026-08-28 13:06:25 +0530

    Time: 0 ms (100%), Space: 8.9 MB (67.47%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,27 +3,33 @@ public:
     bool isValid(string s) {
         stack<char> st;
 
-        for(char ch : s) {
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s[i];
 
-            if(ch == '(' || ch == '[' || ch == '{') {
+            if (ch == '(' || ch == '[' || ch == '{') {
                 st.push(ch);
             }
             else {
-                if(st.empty()) {
+                if (st.empty()) {
                     return false;
                 }
-                if(ch == ')' && st.top() != '(') {
+
+                if (ch == ')' && st.top() != '(') {
                     return false;
                 }
-                if(ch == ']' && st.top() != '[') {
+
+                if (ch == ']' && st.top() != '[') {
                     return false;
                 }
-                if(ch == '}' && st.top() != '{') {
+
+                if (ch == '}' && st.top() != '{') {
                     return false;
                 }
+
                 st.pop();
             }
         }
+
         return st.empty();
     }
 };
